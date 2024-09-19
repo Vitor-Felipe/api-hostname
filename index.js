79ddfd5c7fcea144b68eec36c3b630f3bb82ab7c
Vitor-Felipe <contatovitorf@gmail.com>
2024-09-19 10:56:22 -0300

    fix(ip): get ip addres]

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
-// Rota para capturar hostname do cliente
-app.get('/', (req, res) => {
-    const clientHostname = req.hostname; // Obtém o hostname do cliente
 
+app.get('/', (req, res) => {
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     res.json({
-        message: 'Hostname capturado!',
-        hostname: clientHostname,
-        server: os.hostname() // Mostra também o hostname do servidor, apenas para comparação
+        message: 'Endereço IP do cliente capturado!',
+        ip: clientIp,
     });
 });
+
 
 // Inicializa o servidor
 app.listen(port, () => {
